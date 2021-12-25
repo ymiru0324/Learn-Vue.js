6794866d8287cbe7171a6874312705a547cf3abb
DESKTOP-5BSR2KC\YM <ymiru0324@naver.com>
2021-12-25 16:55:51 +0900

    build: SimpleTodo-Vue.js

---
+++ SimpleTodo-Vue.js/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app" class="container">
+    <h1 class="text-center">Todo App</h1>
+    <input 
+      v-model="todoText"
+      type="text" 
+      class="w-100 p-2" 
+      placeholder="Type todo"
+      @keyup.enter="addTodo"
+    >
+    <hr>
+    <Todo
+      v-for="todo in todos" 
+      :key="todo.id" 
+      :todo="todo"
+      @toggle-checkbox="toggleCheckbox"
+      @click-delete="deleteTodo"
+    />
+
+    {{ todos }}
+  </div>
+</template>
+
+<script>
+import Todo from '@/components/Todo.vue';
+
+export default {
+  components: {
+    Todo
+  },
+  data() {
+    return {
+      todoText: '',
+      todos: [
+        { id: 1, text: 'buy a car', checked: false },
+        { id: 2, text: 'play game', checked: false },
+      ]
+    }
+  },
+  methods: {
+    deleteTodo(id) {
+      // const index = this.todos.findIndex(todo => {
+      //   return todo.id == id;
+      // });
+      // this.todos.splice(index, 1);
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(e) {
+      this.todos.push({
+        id: Math.random(),
+        text: e.target.value,
+        checked: false
+      });
+      this.todoText = '';
+    },
+    toggleCheckbox({id, checked}) {
+      const index = this.todos.findIndex(todo => {
+        return todo.id == id;
+      });
+      this.todos[index].checked = checked;
+    }
+  }
+}
+</script>
