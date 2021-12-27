f2567cae35d59618e2f6904bd5d38d7c3300e01b
DESKTOP-5BSR2KC\YM <ymiru0324@naver.com>
2021-12-27 21:55:18 +0900

    build: Vuex

--- SimpleTodo-Vue.js/src/App.vue
+++ SimpleTodo-Vue.js/src/App.vue
@@ -1,40 +1,34 @@
 <template>
   <div id="app" class="container">
     <h1 class="text-center">Todo App</h1>
-    <input 
-      v-model="todoText"
-      type="text" 
-      class="w-100 p-2" 
-      placeholder="Type todo"
-      @keyup.enter="addTodo"
-    >
+    <CompletedTodo />
+    <AddTodo @add-todo="addTodo" />
     <hr>
-    <Todo
-      v-for="todo in todos" 
-      :key="todo.id" 
-      :todo="todo"
+    <TodoList 
       @toggle-checkbox="toggleCheckbox"
       @click-delete="deleteTodo"
     />
-
-    {{ todos }}
   </div>
 </template>
 
 <script>
-import Todo from '@/components/Todo.vue';
+import TodoList from '@/components/TodoList.vue';
+import AddTodo from '@/components/AddTodo.vue';
+import CompletedTodo from '@/components/CompletedTodo.vue';
 
 export default {
   components: {
-    Todo
+    TodoList,
+    AddTodo,
+    CompletedTodo
   },
   data() {
     return {
       todoText: '',
-      todos: [
-        { id: 1, text: 'buy a car', checked: false },
-        { id: 2, text: 'play game', checked: false },
-      ]
+      // todos: [
+      //   { id: 1, text: 'buy a car', checked: false },
+      //   { id: 2, text: 'play game', checked: false },
+      // ]
     }
   },
   methods: {
@@ -45,10 +39,10 @@ export default {
       // this.todos.splice(index, 1);
       this.todos = this.todos.filter(todo => todo.id !== id);
     },
-    addTodo(e) {
+    addTodo(value) {
       this.todos.push({
         id: Math.random(),
-        text: e.target.value,
+        text: value,
         checked: false
       });
       this.todoText = '';
---
+++ SimpleTodo-Vue.js/src/store/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state: {
+        todos: [
+            { id: 1, text: 'buy a car', checked: false },
+            { id: 2, text: 'play game', checked: false },
+          ]
+    },
+    mutations: {
+
+    },
+    actions: {
+
+    },
+    getters: {
+        
+    }
+});
