bb92f86710cfb7a931b8a2d205882883b8dd492a
yongmin <ymiru0324@naver.com>
2022-02-06 16:55:46 +0900

    update: vue-todo

--- Intermediate-Vue.js/vue-todo/src/components/TodoList.vue
+++ Intermediate-Vue.js/vue-todo/src/components/TodoList.vue
@@ -2,7 +2,7 @@
     <div>
         <ul>
             <li 
-                v-for="(todoItem, index) in todoItems" 
+                v-for="(todoItem, index) in propsdata" 
                 class="shadow"
                 v-bind:key="todoItem.item"
             >
@@ -23,11 +23,7 @@
 
 <script>
 export default {
-    data: function() {
-        return {
-            todoItems: []
-        }
-    },
+    props: ['propsdata'],
     methods: {
         removeTodo: function(todoItem, index) {
             console.log(todoItem, index);
@@ -41,15 +37,6 @@ export default {
             localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
         }
     },
-    created: function() {
-        if (localStorage.length > 0) {
-            for (var i = 0; i < localStorage.length; i++) {
-                if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
-                    this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-                }
-            }
-        }
-    }
 }
 </script>
 
