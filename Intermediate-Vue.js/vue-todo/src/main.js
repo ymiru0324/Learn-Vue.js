6e869c562724672fc185463bd84197b14af20530
ymiru0324 <ymiru0324@naver.com>
2023-03-21 22:14:43 +0900

    feat: Vuex

--- Intermediate-Vue.js/vue-todo/src/main.js
+++ Intermediate-Vue.js/vue-todo/src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-Vue.config.productionTip = false
+import { store } from './store/store'
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  store,
+  render: h => h(App)
+})
