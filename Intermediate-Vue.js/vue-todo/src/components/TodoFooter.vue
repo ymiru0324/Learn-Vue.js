f555bd04dff52b7506baf8b4490423596e1e85a8
ymiru0324 <ymiru0324@naver.com>
2023-03-26 18:16:47 +0900

    rafact: mapMutations

--- Intermediate-Vue.js/vue-todo/src/components/TodoFooter.vue
+++ Intermediate-Vue.js/vue-todo/src/components/TodoFooter.vue
@@ -7,11 +7,13 @@
 </template>
 
 <script>
+import { mapMutations } from 'vuex';
+
 export default {
   methods: {
-    clearTodo() {
-      this.$store.commit('clearAllItems');
-    }
+    ...mapMutations({
+      clearTodo: 'clearAllItems'
+    })
   }
 }
 </script>
