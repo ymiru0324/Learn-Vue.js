da92903273214d01285af9b9d6ad69c74ef66aa2
ymiru0324 <ymiru0324@naver.com>
2023-03-27 21:53:30 +0900

    feat: lesson-4

--- TypeScript-With-Vue.js/src/App.vue
+++ TypeScript-With-Vue.js/src/App.vue
@@ -1,16 +1,17 @@
 <template>
  <div class="app">
-  <p>{{ jobs[0].location }}</p>
+    <JobList :jobs="jobs" />
  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, ref, toRefs } from 'vue';
-import Job from './types/Job'
+import { defineComponent, ref } from 'vue';
+import Job from '@/types/Job'
+import JobList from './components/JobList.vue'
 
 export default defineComponent({
   name: 'App',
-  components: {},
+  components: { JobList },
   setup() {
     const jobs = ref<Job[]>([
       { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
